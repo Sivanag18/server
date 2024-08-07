9be9953d28c01b8f4b12a31a40e8f53e26d5f43f
SivaNag Bhavanam <sivanagbhavanam@gmail.com>
2024-08-07 18:13:42 +0530

    Update the link

--- database/db.js
+++ database/db.js
@@ -3,8 +3,16 @@ import mongoose from 'mongoose';
 const DBConnection = async () => {
     const mongoUri = process.env.MONGO_URI; 
 
+    if (!mongoUri) {
+        console.error('MONGO_URI environment variable not set');
+        process.exit(1);
+    }
+
     try {
-        await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Error while connecting with the database', error.message);
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors(
 ));
 app.use('/', router);
 
-const  PORT = 8000;
+const  PORT =  process.env.PORT || 8000;
 
 DBConnetion();
 
